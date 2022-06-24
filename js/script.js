e2ffb329d17d7765f56a8234778550ebc621f5d7
Alejandro <alejandroorozcomunoz@gmail.com>
2022-06-23 20:00:02 -0500

    Dojonary

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function elimina() {
+    var elementob1 = document.querySelector('.b1'); //Seleccionamos aquel elemento que tenga la clase cookie
+    elementob1.remove();
+} 
+
+function cambiar (element) {
+    element.innerText = "Sign Out";
+}
+
